3e5c80d4f0d2262ceb9964653415c1085b68b8c7
peru <perugoal2@gmail.com>
2021-03-23 22:26:24 +0530

    Deplyment Changes

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t perugoal1/multi-client:latest -t perugoal1/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t perugoal1/multi-server:latest -t perugoal1/multi-server:$SHA -f ./server/Dockerfilev ./server
+docker build -t perugoal1/multi-worker:latest -t perugoal1/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push perugoal1/multi-client:latest
+docker push perugoal1/multi-client:$SHA
+docker push perugoal1/multi-server:latest
+docker push perugoal1/multi-server:$SHA
+dokcer push perugoal1/multi-worker:latest
+dokcer push perugoal1/multi-worker:$SHA
+
+
+kubectl apply -f k8s
+
+kubectl set image deployments/server-deployment server=perugoal1/multi-server:$SHA
+kubectl set image deployments/client-deployment client=perugoal1/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=perugoal1/multi-worker:$SHA
